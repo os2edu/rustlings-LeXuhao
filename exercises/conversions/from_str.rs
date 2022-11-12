876cffacd7c1712e7e72649c72e1efd2ad0d08a5
system <896194379@qq.com>
2022-11-12 22:28:04 +0800

    rustlings

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -9,6 +9,8 @@
 use std::num::ParseIntError;
 use std::str::FromStr;
 
+use ParsePersonError::{BadLen, Empty, NoName, ParseInt};
+
 #[derive(Debug, PartialEq)]
 struct Person {
     name: String,
@@ -28,8 +30,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -43,9 +43,28 @@ enum ParsePersonError {
 // As an aside: `Box<dyn Error>` implements `From<&'_ str>`. This means that if you want to return a
 // string error message, you can do so via just using return `Err("my error message".into())`.
 
+// Using ParsePersonError::{Empty, BadLen, NoName, ParseInt} above
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            Err(Empty) // i.e., Err(ParsePersonError::Empty)
+        } else {
+            let p: Vec<&str> = s.split(',').collect();
+            if p.len() != 2 {
+                Err(BadLen)
+            } else if p[0].len() == 0 {
+                Err(NoName)
+            } else {
+                match p[1].parse::<usize>() {
+                    Ok(a) => Ok(Person {
+                        name: p[0].to_string(),
+                        age: a,
+                    }),
+                    Err(a) => Err(ParseInt(a)),
+                }
+            }
+        }
     }
 }
 
