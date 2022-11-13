9d844ee5e84b5531dafdf9e7c4117d85489cf2d2
system <896194379@qq.com>
2022-11-13 15:08:27 +0800

    rustlings

--- exercises/vecs/vecs2.rs
+++ exercises/vecs/vecs2.rs
@@ -6,7 +6,6 @@
 //
 // Execute `rustlings hint vecs2` or use the `hint` watch subcommand for a hint.
 
-
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for i in v.iter_mut() {
         // TODO: Fill this up so that each element in the Vec `v` is
@@ -19,11 +18,13 @@ fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
 }
 
 fn vec_map(v: &Vec<i32>) -> Vec<i32> {
-    v.iter().map(|num| {
-        // TODO: Do the same thing as above - but instead of mutating the
-        // Vec, you can just return the new number!
-        2 * num
-    }).collect()
+    v.iter()
+        .map(|num| {
+            // TODO: Do the same thing as above - but instead of mutating the
+            // Vec, you can just return the new number!
+            2 * num
+        })
+        .collect()
 }
 
 #[cfg(test)]
